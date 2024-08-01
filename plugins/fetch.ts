9ea125682e4ad4a2af981d288f464fcd12bd8921
datnguyen.7031 <datnguyen.7031@gmail.com>
2024-08-01 16:19:35 +0700

    refactor: update fetch plugin to use accessToken instead of userAuth

--- plugins/fetch.ts
+++ plugins/fetch.ts
@@ -1,15 +1,12 @@
 export default defineNuxtPlugin(() => {
-  const userAuth = useCookie('access_token')
+  const accessToken = useCookie('access_token')
   const config = useRuntimeConfig()
 
   const fetch = $fetch.create({
     baseURL: config.app.baseURL ?? 'https://api.nuxt.com',
     onRequest({ options }) {
-      if (userAuth.value) {
-        // Add Authorization header
-        options.headers = options.headers || {}
-        options.headers = options.headers || {}
-        ;(options.headers as Record<string, string>)['Authorization'] = `Bearer ${userAuth.value}`
+      if (accessToken.value) {
+        options.headers = { ...options.headers, Authorization: `Bearer ${accessToken.value}` }
       }
     },
     onResponse() {
